ac8e49eb210507b8cda75638c401f1bac0bd8c89
MadySankhon <madysankhon823@gmail.com>
2022-06-26 19:32:09 +0000

    push final

--- script-exercice03.js
+++ script-exercice03.js
@@ -13,9 +13,6 @@ btnCalculate.addEventListener("click", (e) => {
 			rateValue = radiosbtn[i].value;
 		}
 	}
-	// console.log(quantityValue);
-	// console.log(priceValue);
-	// console.log(parseFloat(rateValue));
 	const amountValue = quantityValue * priceValue;
 	const totalValue = amountValue + amountValue * parseFloat(rateValue);
 	amount.value = amountValue;
@@ -23,41 +20,3 @@ btnCalculate.addEventListener("click", (e) => {
 	console.log(amountValue);
 	console.log(totalValue);
 });
-// let TauxRadio = document.getElementById("btnRatio");
-// let prixSelect = document.querySelector("select");
-// let PrixHt = document.getElementById("prix");
-// let prixProduit = null;
-
-// TauxRadio.addEventListener("change", function rValTaux() {
-// 	for (let i = 0; i < tauxRadio.length; i++) {
-// 		if (tauxRadio[i].checked) {
-// 			// console.log(tauxRadio[i].value);
-// 			return tauxRadio[i].value;
-// 		}
-// 	}
-// });
-// // let valTaux = rValTaux;
-// // console.log(rValTaux());
-
-// prixSelect.addEventListener("change", function rPrixUnitaire() {
-// 	PrixHt.value = this.value;
-// 	var prixProduit = Number(PrixHt.value);
-// 	// console.log(prixProduit);
-// 	return prixProduit;
-// });
-
-// let valPrixProduit = valPrixUnitaire();
-// rPrixUnitaire();
-// alert(prixProduit);
-
-// var qt = document.getElementById('quantite');
-// var ph = document.getElementById('prix');
-// var montant = document.getElementById('montant')
-
-//  function calculer(qt, ph, montant) {
-//     ph.value = this.value;
-//     montant = Number(qt.value * ph.value);
-//     return (montant);
-
-//  }
-//   console.log(calculer);
